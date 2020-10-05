9415ea8bfb71b1824f00b928ceabdc607274692c
abarnola <abarn067@fiu.edu>
2020-10-04 22:44:11 -0400

    fix global variable usage in main.py

--- main.py
+++ main.py
@@ -14,14 +14,22 @@ def generateProcesses(n):
         at.append(i+1)
         bt.append(random.randint(5, 15))
 
+def clearProcesses():
+    global bt, at, ids, wt
+    bt = []
+    at = []
+    ids = []
+    wt = []
+
 x = int(input('times to run algorithms: '))
 for i in range(x):
     n = random.randint(5, 100)
     print('n = ' + str(n))
     generateProcesses(n)
-    fcfs(n, ids, bt, at)
+    #fcfs(n, ids, bt, at)
+    print(bt)
     rr(n, ids, bt, 2)
     # Need to fix sjf
-    # sjf(n, ids, bt, at)
-
+    #sjf(n, ids, bt, at)
+    clearProcesses()
 
